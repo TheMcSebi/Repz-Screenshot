b08d3c7f2768d2e14da891567b7b15f877162c66
tccr <tccr@rmarkdev.de>
2015-11-21 19:55:27 +0100

    Add DisposableTimeout

---
+++ RepzScreenshot/RepzScreenshot/Extension/DisposableTimeout.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace RepzScreenshot.Extension
+{
+    //https://stackoverflow.com/questions/21468137/async-network-operations-never-finish
+    public sealed class DisposableScope : IDisposable
+    {
+        private readonly Action _closeScopeAction;
+        public DisposableScope(Action closeScopeAction)
+        {
+            _closeScopeAction = closeScopeAction;
+        }
+        public void Dispose()
+        {
+            _closeScopeAction();
+        }
+    }
+
+    public static class DisposableTimeout
+    {
+        public static IDisposable SetTimeout(this IDisposable disposable, TimeSpan timeSpan)
+        {
+            var cancellationTokenSource = new CancellationTokenSource(timeSpan);
+            var cancellationTokenRegistration = cancellationTokenSource.Token.Register(disposable.Dispose);
+            return new DisposableScope(
+                () =>
+                {
+                    cancellationTokenRegistration.Dispose();
+                    cancellationTokenSource.Dispose();
+                    disposable.Dispose();
+                });
+        }
+
+    }
+}
