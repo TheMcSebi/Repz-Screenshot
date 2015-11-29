6e111893b66912f96ff44ee43b8aa8f525720e35
tccr <tccr+github@rmarkdev.de>
2015-11-29 01:02:33 +0100

    Fix ParameterCommand

--- RepzScreenshot/RepzScreenshot/ViewModel/ParameterCommand.cs
+++ RepzScreenshot/RepzScreenshot/ViewModel/ParameterCommand.cs
@@ -6,9 +6,9 @@ namespace RepzScreenshot.ViewModel
     class ParameterCommand<T> : Command
     {
         private readonly Action<T> _action;
-        private readonly Func<bool> _condition;
+        private readonly Func<T, bool> _condition;
 
-        public ParameterCommand(Action<T> action, Func<bool> condition)
+        public ParameterCommand(Action<T> action, Func<T, bool> condition)
         {
             
             _action = action;
@@ -19,8 +19,11 @@ namespace RepzScreenshot.ViewModel
 
         public override bool CanExecute(object parameter)
         {
-
-            return _condition();
+            if (typeof(T) != parameter.GetType())
+            {
+                throw new ArgumentException("Parameter must be of type " + typeof(T));
+            }
+            return _condition((T)parameter);
         }
 
         public override void Execute(object parameter)
